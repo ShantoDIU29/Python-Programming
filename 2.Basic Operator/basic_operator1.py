e1a80eb3af3369bbc38b5dd92f56646463cb138b
shanto roy <shantosr3559@gmail.com>
2025-08-17 22:01:35 +0600

    Basic Operator

---
+++ 2.Basic Operator/basic_operator1.py
@@ -0,0 +1,54 @@
+#basic data type
+print(10+3)
+print(10-3)
+print(10*3)
+print(10 % 3) # vagsesh
+print(10/3)  # decimal vagfol
+print(10//3)  # integer vagfol
+#Comparison Operator In python (==)
+x=5
+y=5
+print(x==y) #True
+x=5
+y=10
+print(x==y)#False
+#comparrison operator in python
+x = 5
+y = 5
+print(x != y)  # Output: False
+
+x = 5
+y = 10
+print(x != y)  # Output: True
+#greater than(>)
+x = 10
+y = 5
+print(x > y)  # Output: True
+
+x = 5
+y = 10
+print(x > y)  # Output: False
+#less than(<)
+x = 10
+y = 5
+print(x < y)  # Output: False
+
+x = 5
+y = 10
+print(x < y)  # Output: True
+#Greater than equal to(>=)
+x = 10
+y = 5
+print(x >= y)  # Output: True
+
+x = 5
+y = 5
+print(x >= y)  # Output: True
+#less than equal to
+x = 10
+y = 5
+print(x <= y)  # Output: False
+
+x = 5
+y = 5
+print(x <= y)  # Output: True
