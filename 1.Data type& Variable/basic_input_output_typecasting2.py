a5ee2f7e2f6b7e7989de8cbc409a41941df79aa4
shanto roy <shantosr3559@gmail.com>
2025-08-17 21:53:32 +0600

    Datatype&variable

---
+++ 1.Data type& Variable/basic_input_output_typecasting2.py
@@ -0,0 +1,16 @@
+#input output and typecasting
+first_money=input("Enter first ammount: ")
+second_money=input("Enter second amount:")
+print(first_money+second_money)
+# Enter first amount : 100
+# Enter second amount : 200
+# 100200 bcz here string input
+#After typecasting-----
+first_m=input("Enter first amount")
+second_m=input("Enter second Number :")
+print(int(first_m)+int(second_m))
+
+ #OUTPUT
+# Enter first amount : 100
+# Enter second amount : 200
+# 300
---
+++ 1.Data type& Variable/data_types_variable1.py
@@ -0,0 +1,23 @@
+age = 12
+name = "karim"
+is_single = True
+interest = 12.50
+
+print(age, type(age))
+print(name, type(name))
+print(is_single, type(is_single))
+print(interest, type(interest))
+#===================================
+# print(age, type(age)) A single line Comment. Press Ctrl+ /
+
+""" 
+Multi-Line Comment Press Alt + Shift + A
+print(name, type(name))
+print(is_single, type(is_single))
+print(interest, type(interest))
+
+"""
+#F_String
+Coname = 'Tushar'
+age = 23
+print(f"Hello, My name is {name} and I'm {age} years old.")
